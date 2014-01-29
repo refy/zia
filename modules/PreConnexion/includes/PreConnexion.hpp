aea69f0cc1db77d99ce00df6cc7a1445f3a66036
herve_c <herve_c@localhost.localdomain>
2014-01-29 13:45:23 +0100

    Commit du module PreConnexion

--- modules/PreConnexion/includes/PreConnexion.hpp
+++ modules/PreConnexion/includes/PreConnexion.hpp
@@ -1,10 +1,20 @@
 #ifndef		__PRECONNEXION_HPP__
 #define		__PRECONNEXION_HPP__
 
+
+#include	<iostream>
+#include	<fstream>
+#include	<ctime>
+#include	<set>
+#include	<mutex>
+
 #include	"AModule.hpp"
 #include	"ILogger.hpp"
 #include	"IConnexion.hpp"
 #include	"Error.hpp"
+
+#define		PRECONNEXION_BLACKLIST_FILE	"blacklist.txt"
+#define		PRECONNEXION_T_RELOAD_BLACKLIST	60
 
 class	PreConnexion : public apimeal::AModule
 {
@@ -23,6 +33,13 @@ public:
 
 public:
   void	preConnexion(apimeal::IConnexion *c, apimeal::Error & e);
+  
+protected:
+	void	load_blacklist();
+  
+	time_t			_t_last_blacklist;
+	std::set<std::string>	_blacklist;
+	std::mutex		_mutex;
 };
 
 #endif
--- modules/PreConnexion/sources/PreConnexion.cpp
+++ modules/PreConnexion/sources/PreConnexion.cpp
@@ -3,7 +3,9 @@
 
 PreConnexion::PreConnexion(apimeal::ILogger *log)
   : AModule(log), _version(0, 1), _name("PreConnexion-Blacklist")
-{}
+{
+	load_blacklist();
+}
 
 PreConnexion::~PreConnexion()
 {}
@@ -21,11 +23,73 @@ const std::string &			PreConnexion::getName() const
   return (this->_name);
 }
 
+
+/**
+** \fn void PreConnexion::preConnexion(apimeal::IConnexion *c, apimeal::Error & e)
+** \brief Verifie que la connexion client n'est pas dans la blacklist
+**
+** \param c Connexion du client a verifier
+** \param e Error a declencher si le client est blackliste
+** \return Ne retourne rien mais prepare l'erreur en cas de probleme
+*/
 void	PreConnexion::preConnexion(apimeal::IConnexion *c, apimeal::Error & e)
 {
-  e.IsError = true;
-  e.Message += "Pre Connexion.\n";
+	if (time(0) > this->_t_last_blacklist)
+		load_blacklist();
+	if (c == 0)
+	{
+		e.IsError = true;
+		e.Message += "Bad pointer\n";
+	}
+	else
+	{
+		std::string	ip_tmp = c->getIp();
+		std::string	hostname_tmp = c->getHostname();
+		
+		this->_mutex.lock();
+		for (std::set<std::string>::const_iterator it=this->_blacklist.begin(); it!=this->_blacklist.end(); ++it)
+		{
+			if (((*it) == ip_tmp) || ((*it) == hostname_tmp))
+			{
+				e.IsError = true;
+				e.Message += "Blacklisted client\n";
+				this->_mutex.unlock();
+				return ;
+			}
+		}
+		e.IsError = false;
+		e.Message += "OK\n";
+		this->_mutex.unlock();
+	}
 }
+
+
+/**
+** \fn void PreConnexion::load_blacklist()
+** \brief Gere le rechargement de la blacklist
+**
+** \return Retourne rien
+*/
+void	PreConnexion::load_blacklist()
+{
+	std::ifstream	file(PRECONNEXION_BLACKLIST_FILE);
+
+	this->_mutex.lock();
+	_blacklist.clear();
+	if (file)
+	{
+		std::string	line;
+ 
+		while (std::getline(file, line))
+		{
+			if (line.size() > 0)
+        			_blacklist.insert(line);
+        	}
+	}
+	this->_t_last_blacklist = time(0) + PRECONNEXION_T_RELOAD_BLACKLIST;
+	this->_mutex.unlock();
+}
+
 
 extern "C" DLLAPIMEAL	apimeal::AModule*	LoadModule(apimeal::ILogger *log)
 {
