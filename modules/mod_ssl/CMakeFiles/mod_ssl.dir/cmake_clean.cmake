b3be3ece3e6180c78a64bb05b467c1aa23962647
Ataraxis <ataraxis@mint>
2014-02-04 12:58:24 +0100

    compile

---
+++ modules/mod_ssl/CMakeFiles/mod_ssl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mod_ssl.dir/sources/mod_ssl.cpp.o"
+  "libmod_ssl.pdb"
+  "libmod_ssl.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mod_ssl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
